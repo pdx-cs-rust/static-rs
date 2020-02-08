c140154e80ff8f89d2fea066b3ad07d959a8274b
Bart Massey <bart.massey@gmail.com>
2020-02-07 22:12:27 -0800

    cleaned up build script

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 fn main() {
+    let name = "writeln_ffi_c";
+    let cname = format!("src/{}.c", name);
     cc::Build::new()
-        .file("src/writeln_ffi_c.c")
-        .compile("writeln_ffi_c");
-    println!("cargo:rerun-if-changed=src/writeln_ffi_c.c");
+        .file(&cname)
+        .compile(name);
+    println!("cargo:rerun-if-changed={}", cname);
 }
